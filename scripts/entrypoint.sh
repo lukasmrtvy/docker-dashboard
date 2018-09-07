b89cb5c5dc566d9454feddb5e30190aa69e9e24e
Lukas M <lukas.mrtvy@gmail.com>
2018-09-07 18:32:08 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,9 +2,11 @@
 
 su -s /bin/bash -c "/opt/check/exec.sh" "${user}"
 
-CRON="${CUSTOM_CRON:-*/5 * * * *}"
+webui_user="${custom_webui_user:-"admin"}"
+webui_pwd="${custom_webui_pwd:-"admin"}"
+cron="${custom_cron:-"*/5 * * * *"}"
 
-echo "${CRON}  /opt/check/exec.sh" >> /etc/crontabs/root 
+echo "${cron}  /opt/check/exec.sh" >> /etc/crontabs/root 
 
 
 exec $@
