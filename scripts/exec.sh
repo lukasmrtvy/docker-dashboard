b0b9c4f71532d35fcd9e20be5f4e0c5a391a741a
Lukas M <lukas.mrtvy@gmail.com>
2018-09-07 17:41:07 +0200

    Update exec.sh

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "$(date -I'seconds') INFO Loading data from docker.socket"
+
 command=($(curl -s --unix-socket /var/run/docker.sock http:/localhost/containers/json | jq -r '.[].Id'))
 result="result.json"
 echo '{"data":[]}' > "$result"
@@ -8,15 +10,14 @@ echo '{"data":[]}' > "$result"
 for p in "${command[@]}"
 do
 
-
   j=$(curl -s --unix-socket /var/run/docker.sock http:/localhost/containers/${p}/json)
   name=$(jq -r '.Name' <<< "$j")
   state=$(jq -r '.State.Status' <<< "$j")
 
-
-
 jq --arg arg_name "$name" \
   --arg arg_state "$state" \
         '.data += [[ $arg_name, $arg_state  ]]' "$result" | sponge "$result"
+        
+echo "$(date -I'seconds') INFO Loading data from docker.socket DONE"
 
 done
